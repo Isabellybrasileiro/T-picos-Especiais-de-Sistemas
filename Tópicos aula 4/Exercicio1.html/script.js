590088330716b440fb8b85046a40e1d948e90c5f
Isabellybrasileiro <83516926+Isabellybrasileiro@users.noreply.github.com>
2022-03-31 10:58:21 -0300

    Add files via upload

---
+++ Tópicos aula 4/Exercicio1.html/script.js
@@ -0,0 +1,12 @@
+function numero() {
+    let para = document.createElement('p');
+    para.textContent = '1, 2, 3, 4, 5, 6, 7, 8, 9, 10';
+    document.body.appendChild(para);
+    }
+    
+function explicacao() {
+        let para = document.createElement('p');
+        para.textContent = 'Débito técnico é quando você escolhe implementar uma funcionalidade – ou parte dela – com código de qualidade inferior para ganhar mais tempo.';
+        document.body.appendChild(para);
+        }
+    
